5faa3f241dff75f1c1381eda438735330b0c0c50
chenliang <2237654972@qq.com>
2019-12-31 12:46:52 +0800

    测试gitHub

--- app/controller/user.js
+++ app/controller/user.js
@@ -6,67 +6,65 @@ class UserController extends Controller {
   async index() {
 
 
+    let userList = await this.service.user.getUserList();
+    console.log(userList);
 
-    var userList=await this.service.user.getUserList();
-    console.log(userList);       
-  
 
-    this.ctx.body='我是用户页面';
-    
+    this.ctx.body = '我是用户页面';
+
   }
 
   async addUser() {
-    //增加数据
+    // 增加数据
 
-    var user=new this.ctx.model.User({
-        username:'李四',
-        password:'123456'
+    let user = new this.ctx.model.User({
+      username: '李四',
+      password: '123456',
 
     });
 
-    var result=await user.save();
-    console.log(result)
+    let result = await user.save();
+    console.log(result);
 
 
+    this.ctx.body = '增加用户成功';
 
-    this.ctx.body='增加用户成功';
-    
   }
 
   async editUser() {
-    //增加数据
+    // 增加数据
 
 
     await this.ctx.model.User.updateOne({
-        "_id":"5b84d4405f66f20370dd53de"
-    },{
-      username:"哈哈哈",
-      password:'1234'
-    },function(err,result){
+      _id: '5b84d4405f66f20370dd53de',
+    }, {
+      username: '哈哈哈',
+      password: '1234',
+    }, function(err, result) {
 
-      if(err){
+      if (err) {
         console.log(err);
         return;
       }
-      console.log(result)
-    })
+      console.log(result);
+    });
 
-    this.ctx.body='修改用户成功';
-    
+    this.ctx.body = '修改用户成功';
+
   }
 
   async removeUser() {
-    //增加数据
+    // 增加数据
 
-    var rel =await this.ctx.model.User.deleteOne({"_id":"5b84d4b3c782f441c45d8bab"});
+    let rel = await this.ctx.model.User.deleteOne({ _id: '5b84d4b3c782f441c45d8bab' });
 
     cnsole.log(rel);
 
-    this.ctx.body='删除用户成功';
-    
+    this.ctx.body = '删除用户成功';
+
   }
 }
 
 module.exports = UserController;
 
-//mongoose aggregate实现关联表的查询
+// mongoose aggregate实现关联表的查询
