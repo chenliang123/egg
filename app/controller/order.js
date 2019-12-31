041d787100e5329d8c86ea21d798bef02b04870f
chenliang <2237654972@qq.com>
2019-12-31 11:17:12 +0800

    order.js文件格式修复

--- app/controller/order.js
+++ app/controller/order.js
@@ -4,32 +4,32 @@ const Controller = require('egg').Controller;
 
 class OrderController extends Controller {
   async index() {
-    
-    //实现关联查询
+
+    // 实现关联查询
 
 
     // var orderResult=await this.ctx.model.Order.find({});
 
 
-    var orderResult=await this.ctx.model.Order.aggregate([
+    const orderResult = await this.ctx.model.Order.aggregate([
 
-        {
+      {
 
-            $lookup:{
-                from:'order_item',
-                localField:'order_id',
-                foreignField:'order_id',
-                as:'items'
-            }
+        $lookup: {
+          from: 'order_item',
+          localField: 'order_id',
+          foreignField: 'order_id',
+          as: 'items',
         },
-        {
-            $match:{"all_price":{$gte:90}}
-        }
+      },
+      {
+        $match: { all_price: { $gte: 90 } },
+      },
 
     ]);
 
 
-    this.ctx.body=orderResult;
+    this.ctx.body = orderResult;
 
 
   }
